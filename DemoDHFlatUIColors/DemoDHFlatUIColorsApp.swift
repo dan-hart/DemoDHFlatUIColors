467e5e90ff27c9619495f0fa6d3be95668c86fc7
Dan Hart <dan-hart@users.noreply.github.com>
2024-11-11 13:06:35 -0600

    Initial Commit

---
+++ DemoDHFlatUIColors/DemoDHFlatUIColorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoDHFlatUIColorsApp.swift
+//  DemoDHFlatUIColors
+//
+//  Created by Dan Hart on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct DemoDHFlatUIColorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
